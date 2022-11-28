0b7005dae51115f729388b8f71573964a31f4ded
sanghee <wjdtkdgml7352@daum.net>
2022-11-28 13:36:17 +0900

    userDto 클래스에 id 추가, @Builder 추가

--- src/main/java/com/example/hospitalreview/domain/UserDto.java
+++ src/main/java/com/example/hospitalreview/domain/UserDto.java
@@ -1,10 +1,16 @@
 package com.example.hospitalreview.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
 @AllArgsConstructor
 @Getter
-public class UserDto {
+public class UserDto { // transeform을 위한 dto
+    private Long id;
     private String userName;
     private String password;
     private String email;
