3c647624e14b0f3a7edcd7cf71f3950f5412de84
sanghee <wjdtkdgml7352@daum.net>
2022-11-28 13:38:07 +0900

    userService 중복 체크 후 회원 저장 로직

--- src/main/java/com/example/hospitalreview/service/UserService.java
+++ src/main/java/com/example/hospitalreview/service/UserService.java
@@ -1,13 +1,30 @@
 package com.example.hospitalreview.service;
 
+import com.example.hospitalreview.domain.User;
 import com.example.hospitalreview.domain.UserDto;
 import com.example.hospitalreview.domain.dto.UserJoinRequest;
+import com.example.hospitalreview.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
-
+    private final UserRepository userRepository;
     public UserDto join(UserJoinRequest request) {
-        return new UserDto("","","");
+        // 비즈니스 로직 - 회원 가입
+
+        // 회원 userName(id) 중복 check
+
+        // 중복되면 회원가입 불가능 exception발생
+        userRepository.findByUserName(request.getUserName())
+                .orElseThrow(()->new RuntimeException("해당 UserName은 이미 존재합니다."));
+        // 회원가입 .save()
+        User savedUser = userRepository.save(request.toEntity());
+        return UserDto.builder()
+                .id(savedUser.getId())
+                .userName(savedUser.getUserName())
+                .email(savedUser.getEmailAddress())
+                .build();
     }
 }
