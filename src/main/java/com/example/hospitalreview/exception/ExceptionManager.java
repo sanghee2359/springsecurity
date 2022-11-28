a30476709934290c28f76323f1274ae9ca81ae99
sanghee <wjdtkdgml7352@daum.net>
2022-11-28 14:30:45 +0900

    [추가] client에게 예외의 원인을 알려주기 위해 ExceptionManager 생성 - @RestControllerAdvice

---
+++ src/main/java/com/example/hospitalreview/exception/ExceptionManager.java
@@ -0,0 +1,16 @@
+package com.example.hospitalreview.exception;
+
+import com.example.hospitalreview.domain.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionManager {
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<?> runtimeExceptionHandler(RuntimeException e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(Response.error(e.getMessage()));
+    }
+}
