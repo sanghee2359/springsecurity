4e644f1e91bc3fa2cd53e7faffa3f73babad177e
sanghee <wjdtkdgml7352@daum.net>
2022-11-29 14:25:24 +0900

    [추가] 로그인 요청(userName,password), 로그인 응답(token) Dto 작성

---
+++ src/main/java/com/example/hospitalreview/domain/dto/UserLoginResponse.java
@@ -0,0 +1,10 @@
+package com.example.hospitalreview.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class UserLoginResponse {
+    private String token;
+}
