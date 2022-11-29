a5047a168c7b89fe92e96754d3dc176d72c8f9a0
sanghee <wjdtkdgml7352@daum.net>
2022-11-29 14:23:30 +0900

    [추가] 로그인 - id가 유효하지 않을 경우 client에게 보낼 에러코드 작성

--- src/main/java/com/example/hospitalreview/exception/ErrorCode.java
+++ src/main/java/com/example/hospitalreview/exception/ErrorCode.java
@@ -6,7 +6,10 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 @Getter
 public enum ErrorCode {
-    DUPLICATED_USER_NAME(HttpStatus.CONFLICT, "해당 UserName은 이미 존재합니다.");
+    DUPLICATED_USER_NAME(HttpStatus.CONFLICT, "해당 UserName은 이미 존재합니다."),
+    NOT_FOUND(HttpStatus.NOT_FOUND,"UserName Not Found"),
+    INVALID_PASSWORD(HttpStatus.BAD_REQUEST,"");
+    // unAuthorize 오류 페이지
     private HttpStatus status;
     private String message;
 }
