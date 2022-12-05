6675e555cde6b63bef06123470bf46ea7a8c537a
sanghee <wjdtkdgml7352@daum.net>
2022-12-05 11:23:53 +0900

    [추가] 접근 요청(post) 막기

--- src/main/java/com/example/hospitalreview/configuration/SecurityConfig.java
+++ src/main/java/com/example/hospitalreview/configuration/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.hospitalreview.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -17,8 +18,8 @@ public class SecurityConfig {
                 .csrf().disable()
                 .cors().and()
                 .authorizeRequests()
-                .antMatchers("/api/**").permitAll()
                 .antMatchers("/api/v1/users/join", "/api/v1/users/login").permitAll() // join, login은 언제나 가능
+                .antMatchers(HttpMethod.POST, "/api/v1/**").authenticated()
                 .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // jwt사용하는 경우 씀
